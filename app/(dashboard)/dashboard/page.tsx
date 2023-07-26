aeb5574045f56c3baa0117b8d3e0770f2521b367
Nabarun <nicknabarun@gmail.com>
2023-07-26 23:59:24 +0530

    Make db calls and drill props to `ApiDashboard`

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { formatDistance } from "date-fns";
 
 export const metadata: Metadata = {
   title: "Text Similarity API | Dashboard",
@@ -14,6 +15,7 @@ const DashboardPage = async () => {
 
   if (!session) return notFound();
 
+  // valid api key
   const apiKey = await db.apiKey.findFirst({
     where: {
       userId: session.user.id,
@@ -21,9 +23,36 @@ const DashboardPage = async () => {
     },
   });
 
+  // all api keys the user has generated irrespective of being valid
+  const apiKeys = await db.apiKey.findMany({
+    where: { userId: session.user.id },
+  });
+
+  // all requests made by the user with their api keys
+  const userRequests = await db.apiRequest.findMany({
+    where: {
+      apiKeyId: {
+        in: apiKeys.map((key) => key.id),
+      },
+    },
+  });
+
+  const serializableRequests = userRequests.map((req) => ({
+    ...req,
+    timestamp: formatDistance(new Date(req.timestamp), new Date()), // convert timestamp into a more human readable format
+  }));
+
   return (
     <div className='max-w-7xl mx-auto mt-16'>
-      {apiKey ? <ApiDashboard /> : <RequestApiKey />}
+      {apiKey ? (
+        <ApiDashboard
+          session={session}
+          apiKeys={apiKeys}
+          serializableRequests={serializableRequests}
+        />
+      ) : (
+        <RequestApiKey />
+      )}
     </div>
   );
 };
