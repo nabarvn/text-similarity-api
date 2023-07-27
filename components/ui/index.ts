bee348a9438df20d96f8bdee23484d95976d2844
Nabarun <nicknabarun@gmail.com>
2023-07-27 21:33:43 +0530

    Export `Table`

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,6 +1,7 @@
 export { default as Paragraph } from "./Paragraph";
 export { default as Heading } from "./Heading";
 export { default as Button } from "./Button";
+export { default as Table } from "./Table";
 export { DropdownMenu } from "./DropdownMenu";
 export { Toast } from "./Toast";
 export { Tabs } from "./Tabs";
