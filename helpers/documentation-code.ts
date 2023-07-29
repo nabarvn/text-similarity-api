bb6acbe2a5676e4472f3bbbb0d4a35af3915d070
Nabarun <nicknabarun@gmail.com>
2023-07-29 23:59:05 +0530

    Change url

--- helpers/documentation-code.ts
+++ helpers/documentation-code.ts
@@ -1,43 +1,43 @@
 export const nodejs = `const axios = require("axios");
 
 const options = {
-    method: 'POST',
-    url: 'https://similarityapi.com/api/v1/similarity',
-    params: {
-      text1: 'First text',
-      text2: 'Second text'
-    },
-    headers: {
-      'Authorization': 'YOUR_API_KEY',
-    }
-  };
+  method: 'POST',
+  url: 'https://text-similarity-api.nabarun.app/api/v1/text-similarity',
+  params: {
+    text1: 'First text',
+    text2: 'Second text'
+  },
+  headers: {
+    'Authorization': 'YOUR_API_KEY',
+  }
+};
   
 axios.request(options).then(function (response) {
-    console.log(response.data);
+  console.log(response.data);
 }).catch(function (error) {
-    console.error(error);
+  console.error(error);
 });`;
 
 export const python = `import requests
 
-url = 'https://similarityapi.com/api/v1/similarity'
+url = 'https://text-similarity-api.nabarun.app/api/v1/text-similarity'
 api_key = 'YOUR_API_KEY'
 text1 = 'First text'
 text2 = 'Second text'
 
 headers = {
-    'Authorization': api_key
+  'Authorization': api_key
 }
 
 payload = {
-    'text1': text1,
-    'text2': text2
+  'text1': text1,
+  'text2': text2
 }
 
 response = requests.post(url, headers=headers, json=payload)
 
 if response.status_code == 200:
-    data = response.json()
-    print(data)
+  data = response.json()
+  print(data)
 else:
-    print(f'Request failed with status code {response.status_code}')`;
+  print(f'Request failed with status code {response.status_code}')`;
