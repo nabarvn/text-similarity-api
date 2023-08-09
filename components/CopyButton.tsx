e4a7b6c320fb23a94bbec012bb31f5699badccea
Nabarun <nicknabarun@gmail.com>
2023-08-09 14:43:25 +0530

    Implement check icon

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -1,31 +1,38 @@
 "use client";
 
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, useState } from "react";
 import { Button } from "@/components/ui";
 import { toast } from "./ui/Toast";
-import { Copy } from "lucide-react";
+import { Copy, Check } from "lucide-react";
 
 interface CopyButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   valueToCopy: string;
 }
 
 const CopyButton = ({ valueToCopy, className, ...props }: CopyButtonProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+
   return (
     <Button
       {...props}
       onClick={() => {
         navigator.clipboard.writeText(valueToCopy);
+        setIsCopied(true);
 
         toast({
           title: "Copied successfully!",
           message: "API key copied to clipboard.",
           type: "success",
         });
+
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 3000);
       }}
       variant='ghost'
       className={className}
     >
-      <Copy className='h-5 w-5' />
+      {isCopied ? <Check className='h-5 w-5' /> : <Copy className='h-5 w-5' />}
     </Button>
   );
 };
